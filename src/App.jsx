1d98d94d3babe86f5cfe1fe13af8882268909fd0
EmilceAide <emilceaide.t@gmail.com>
2023-03-13 22:47:52 -0500

    form - register

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './component/pure/Login'
+import Register from './component/pure/Register'
+// import Login from './component/pure/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-     <Login />
+      <Register/>
+     {/* <Login /> */}
     </div>
   )
 }
